bb9b25f2cad4d3756ce151e9647559f8045ccbe2
d4tk1dmVn <datkidmun@gmail.com>
2025-07-17 21:14:14 +0200

    Define max length of printing as constant

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,8 +1,9 @@
 module Constants
   SEPARATOR = '**********************************************'.freeze
-  TITLE = "\nODIN'S MACHIAVELLIAN MASTERMIND v1.0.0 - 2025\n".freeze
+  TITLE = "ODIN'S MACHIAVELLIAN MASTERMIND v1.0.0 - 2025".freeze
   NOTCHES = 4
   ROWS = 12
+  # Colors Players choose from
   COLORS = {
     r: "\033[31mRED\033[0m",                # Red text
     o: "\033[38;2;255;165;0mORANGE\033[0m", # Truecolor orange (RGB 255,165,0)
@@ -11,13 +12,14 @@ module Constants
     b: "\033[34mBLUE\033[0m",               # Blue text
     i: "\033[38;5;54mINDIGO\033[0m",        # Indigo with 256-color code (dark blue)
     v: "\033[35mVIOLET\033[0m",             # Magenta (violet)
-    n: "\033[90;40mNONE\033[0m"             # White text on black background (space)
+    n: "\033[90mNONE\033[0m"                # White text on black background (space)
   }.freeze
   HINTS = {
     correct: "\033[32mCORRECT\033[0m",
     almost: "\033[33mALMOST\033[0m",
     incorrect: "\033[31mINCORRECT\033[0m"
   }.freeze
+  # Prompt shown to Players when choosing colors
   COLOR_PROMPT = """
     \033[31m(r)ed\033[0m
     \033[38;2;255;165;0m(o)range\033[0m
@@ -27,4 +29,5 @@ module Constants
     \033[38;5;54m(i)ndigo\033[0m
     \033[35m(v)iolet\033[0m
     \033[90;40m(n)one\033[0m """.freeze
+  MAX_ROW_LENGTH = 80
 end
