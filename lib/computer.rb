78bea4c05d7272480e7cee22733ad55f038ad165
d4tk1dmVn <datkidmun@gmail.com>
2025-08-08 12:51:53 +0200

    Reorganize mastermind solving code

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,18 @@
 require 'set'
 require_relative 'knuth'
+require_relative 'constants'
 
 # Computer class
 class Computer
+  attr_accesor :possible_solutions
+  attr_reader :notches, :all_solutions, :all_scores
+
   include Knuthable
   def initialize(notches)
     @notches = notches
+    @all_solutions = Knuthable.generate_all_solutions
+    @all_scores = Knuthable.generate_all_scores
+    @possible_solutions = @all_solutions.dup
   end
 
   def generate_solution(easy_mode)
@@ -21,7 +28,20 @@ class Computer
   end
 
   def generate_guess_list(solution)
-    Knuthable.solve_mastermind(solution)
+    guess_list = []
+    Constants::ROWS.times do
+      if guess_list.empty
+        color_one = Constants::COLORS.keys[0]
+        color_two = Constants::COLORS.keys[1]
+        [color_one, color_one, color_two, color_two]
+      else
+        self.possible_solutions = Knuthable.purge_less_likely_solutions(possible_solutions, solution, guess_list[-1])
+        current_unused_scores = Knuthable.minimax_step(all_solutions, all_scores, possible_solutions, guess_list)
+        max_score_indices = Knuthable.calculate_max_score_indices(current_unused_scores)
+        Knuthable.calculate_next_guess(all_solutions, possible_solutions, max_score_indices)
+      end
+    end
+    guess_list
   end
 
   private
--- lib/knuth.rb
+++ lib/knuth.rb
@@ -6,6 +6,25 @@ require_relative 'constants'
 # All props and rights go to the owner, catlzy
 
 module Knuthable
+  def self.generate_all_solutions
+    result = []
+    Constants::COLORS.keys.repeated_permutation(Constants::NOTCHES) do |pos_solution|
+      result.append(pos_solution)
+    end
+    result
+  end
+
+  def self.generate_all_scores
+    result = []
+    (Constants::NOTCHES + 1).times do |i|
+      (Constants::NOTCHES - i + 1).times do |j|
+        result << [i, j]
+      end
+    end
+    result.delete_at(result.length - 2)
+    result
+  end
+
   def self.evaluate_guess(solution, guess)
     return unless solution.length == guess.length
 
@@ -39,25 +58,6 @@ module Knuthable
     [colors_in_place, colors_out_of_place]
   end
 
-  def self.generate_all_solutions
-    result = []
-    Constants::COLORS.keys.repeated_permutation(Constants::NOTCHES) do |pos_solution|
-      result.append(pos_solution)
-    end
-    result
-  end
-
-  def self.generate_all_scores
-    result = []
-    (Constants::NOTCHES + 1).times do |i|
-      (Constants::NOTCHES - i + 1).times do |j|
-        result << [i, j]
-      end
-    end
-    result.delete_at(result.length - 2)
-    result
-  end
-
   def self.minimax_step(all_solutions, all_scores, possible_solutions, guess_list)
     current_unused_scores = [] * all_solutions.length
     all_solutions.each do |solution|
@@ -85,7 +85,8 @@ module Knuthable
     indices
   end
 
-  def self.purge_less_likely_solutions(possible_solutions, guess, guess_correctness)
+  def self.purge_less_likely_solutions(possible_solutions, solution, guess)
+    guess_correctness = evaluate_guess(solution, guess)
     purged_solutions = []
     possible_solutions.each do |possible_solution|
       purged_solutions << possible_solution if evaluate_guess(guess, possible_solution) == guess_correctness
@@ -93,7 +94,7 @@ module Knuthable
     purged_solutions
   end
 
-  def self.calculate_next_guess(all_solutions, possible_solutions, max_score_indices)
+  def self.calculate_guess(all_solutions, possible_solutions, max_score_indices)
     result = []
     change = false
     max_score_indices.each do |index|
@@ -105,24 +106,5 @@ module Knuthable
     end
     result = all_solutions[max_score_indices[0]] unless change
     result
-  end
-
-  def self.solve_mastermind(solution)
-    all_solutions = generate_all_solutions
-    possible_solutions = all_solutions.dup
-    all_scores = generate_all_scores
-    guess = [Constants::COLORS.keys[0], Constants::COLORS.keys[0], Constants::COLORS.keys[1], Constants::COLORS.keys[1]]
-    guess_list = [guess]
-    guess_correctness = evaluate_guess(solution, guess)
-
-    while guess_correctness != [Constants::NOTCHES, 0]
-      possible_solutions = purge_less_likely_solutions(possible_solutions, guess, guess_correctness)
-      current_unused_scores = minimax_step(all_solutions, all_scores, possible_solutions, guess_list)
-      max_score_indices = calculate_max_score_indices(current_unused_scores)
-      guess = calculate_next_guess(all_solutions, possible_solutions, max_score_indices)
-      guess_list << guess
-      guess_correctness = evaluate_guess(solution, guess)
-    end
-    guess_list
   end
 end
